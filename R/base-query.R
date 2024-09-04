bc66ec110c30120f40206307ad4ca450fc667b1a
msperlin <marceloperlin@gmail.com>
2024-09-04 14:05:53 -0300

    new fct get_ipos()

--- R/base-query.R
+++ R/base-query.R
@@ -10,7 +10,8 @@ query_api <- function(url) {
   response <- httr::GET(url)
   parse_status_code(response$status_code)
 
-  content <- httr::content(response, "text", encoding = "UTF-8")
+  content <- httr::content(response, "text",
+                           encoding = "UTF-8")
 
   return(content)
 
